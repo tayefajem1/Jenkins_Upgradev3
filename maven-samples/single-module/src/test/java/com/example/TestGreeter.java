9b395553a8218e772bbd90141dffe740b8a0633b
tayefajem1 <53397202+tayefajem1@users.noreply.github.com>
2021-01-19 16:11:52 -0500

    Update TestGreeter.java

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -37,4 +37,11 @@ public class TestGreeter {
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
+ @Test
+  public void greetShouldIncludeGreetingPhrase() {
+    String someone = "Thank you Ola";
+
+    assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
+  } 
+  
 }
